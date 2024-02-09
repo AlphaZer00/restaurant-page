221a5e74e4a8d12b49b639679326cfc0b4beefa0
AlphaZer00 <batudasdemir@gmail.com>
2024-02-09 10:33:35 -0500

    Add function to update DOM according to nav buttons

--- src/index.js
+++ src/index.js
@@ -2,4 +2,26 @@ import loadHomePage from './homePage';
 import loadMenuPage from './menuPage';
 import loadAboutPage from './aboutPage';
 
-loadAboutPage();
+loadHomePage();
+function addNavEventListener() {
+    const homeBtn = document.getElementById('home-button');
+    const menuBtn = document.getElementById('menu-button');
+    const aboutBtn = document.getElementById('about-button');
+
+    homeBtn.addEventListener('click', switchTabs);
+    menuBtn.addEventListener('click', switchTabs);
+    aboutBtn.addEventListener('click', switchTabs);
+}
+
+function switchTabs(e) {
+    content.textContent = '';
+    if (e.target.id === 'home-button') {
+        loadHomePage();
+    } else if (e.target.id === 'menu-button') {
+        loadMenuPage();
+    } else if (e.target.id === 'about-button') {
+        loadAboutPage();
+    }
+}
+
+addNavEventListener();
